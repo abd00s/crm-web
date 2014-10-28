183300e4ccac6ef4ddd2e7da1d2756b7f03c8b51
Abdullah <abd00s@users.noreply.github.com>
2014-10-28 14:52:37 -0400

    links to /contacts

---
+++ contact.rb
@@ -0,0 +1,14 @@
+# Creates an instance of the Contact class
+
+class Contact
+  attr_accessor :id, :first_name, :last_name, :email, :note
+  def initialize(first_name, last_name, email, note)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
+    puts "\e[H\e[2J"
+    puts "Contact succesffully added."
+    puts
+  end
+end
--- crm.rb
+++ crm.rb
@@ -1,6 +1,16 @@
 require 'sinatra'
+require_relative 'contact.rb'
 
 get "/" do 
 	@crm_name = "My CRM"
 	erb :index
+end
+
+get "/contacts" do
+  @contacts = []
+  @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
+  @contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
+  @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
+
+  erb :contacts
 end
