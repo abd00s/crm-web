91dbb8f0288fd232af2af7da41c5a998cee8dddf
Abdullah <abd00s@users.noreply.github.com>
2014-10-28 15:22:09 -0400

    add rolodex

--- crm.rb
+++ crm.rb
@@ -1,16 +1,14 @@
 require 'sinatra'
 require_relative 'contact.rb'
+require_relative 'rolodex.rb'
+
+$rolodex = Rolodex.new
 
 get "/" do 
 	@crm_name = "My CRM"
 	erb :index
 end
 
-get "/contacts" do
-  @contacts = []
-  @contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
-  @contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
-  @contacts << Contact.new("Chris", "Johnston", "chris@bitmakerlabs.com", "Instructor")
-
+get "/contacts" do 
   erb :contacts
 end
---
+++ rolodex.rb
@@ -0,0 +1,18 @@
+# Creates a single Rolodex that stores all our contacts
+
+class Rolodex
+  def initialize
+    @contacts = []
+    @id = 1000
+  end
+
+  def contacts
+    @contacts
+  end
+
+  def add_contact(contact)
+    contact.id = @id
+    @contacts << contact
+    @id += 1
+  end
+end
